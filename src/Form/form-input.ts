94597d490b6169e1c220cd306a3c3ae258586677
Matteo Miotello <matteo.miotello@shellrent.com>
2024-05-28 11:33:42 +0200

    fix tipi input props

--- src/Form/form-input.ts
+++ src/Form/form-input.ts
@@ -1,9 +1,9 @@
-import { ForwardedRef } from "react";
+import { Ref } from "react";
 import { InputValidationHandler } from "..";
 
 export type InputSize = 'xs' | 'sm' | 'lg' | 'md';
 
-export type InputProps<TValue, V = unknown> = { 
+export type InputProps<TValue, V = unknown, Input extends {value?: string} = any> = { 
 	name?: string, 
 	value?: TValue, 
 	onValueChange?: ( value: any ) => void, 
@@ -11,5 +11,5 @@ export type InputProps<TValue, V = unknown> = {
 	error?: string | boolean | undefined | null,
 	validators?: (InputValidationHandler<V> | undefined)[] | undefined,
 	inputSize?: InputSize | undefined,
-	ref: ForwardedRef<HTMLInputElement | HTMLTextAreaElement>
+	ref?: Ref<Input>
 } 
