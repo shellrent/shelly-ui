005f240281948790eeb9cdceaa7124f0a05a29d1
Matteo Bertoldi <matteo.bertoldi@shellrent.com>
2024-07-29 16:56:20 +0200

    aggiunta searchbar a select

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -32,6 +32,7 @@ const Select = forwardRef<HTMLInputElement, SelectProps>( ( {displayFn, value, d
 	const [options, setOptions] = useState<SelectOption[]>( props.options );
 	const {error: err, handleValidation} = useSelectError({err: error, validators});
 	const prevValue = useRef( value );
+	const [searchQuery, setSearchQuery] = useState<string>("");
 
 	useEffect( () => {
 		if ( showEmptyOption ) {
@@ -71,6 +72,11 @@ const Select = forwardRef<HTMLInputElement, SelectProps>( ( {displayFn, value, d
 		setSelectedOption( option );
 	}, [value, options, defaultValue] );
 
+	useEffect( () => {
+		const option = options.find(opt => opt.value == selectedValue);
+		setSelectedOption(option);
+	}, [selectedValue]);
+
 	const onSelectChange = ( value: any ) => {	
 		setSelectedValue( value );
 
@@ -83,6 +89,9 @@ const Select = forwardRef<HTMLInputElement, SelectProps>( ( {displayFn, value, d
 		}
 	};
 
+	const filteredOptions = searchQuery
+		? options.filter(option => option.title.toLowerCase().includes(searchQuery.toLowerCase()))
+		: options;
 
 	const classNames = clsx(
 		'select select-bordered relative w-full cursor-default text-left',
@@ -96,59 +105,61 @@ const Select = forwardRef<HTMLInputElement, SelectProps>( ( {displayFn, value, d
 		inputSize || 'text-base'
 	);
 
-	return  <Listbox ref={ref} value={selectedValue === undefined ? '' : selectedValue }  onChange={onSelectChange} name={name} {...props}>
-		<div className="relative">    
-			<Listbox.Button className={classNames}>
-				<span className="h-full flex items-center truncate overflow-hidden">
-					{
-						(placeholder && selectedOption === undefined) && <span className="text-gray-400 font-normal h-full flex items-center truncate">{placeholder}</span>
-					}
-					{       
-						<span className={ ((showEmptyOption && ( selectedOption?.value === null || selectedOption?.disabled ) ) ? '!text-gray-400' : '') + ' mx-1'}>{ (displayFn && selectedOption) ? displayFn( selectedOption ) : selectedOption?.title } </span>
-					} 
-				</span>
-			</Listbox.Button>
-			<FieldError error={err}></FieldError>
-			<Transition
-				as={Fragment}
-				leave="transition ease-in duration-100"
-				leaveFrom="opacity-100"
-				leaveTo="opacity-0"
-			>
-				<Listbox.Options className="absolute z-30 mt-1 pl-0 max-h-60 w-full overflow-auto rounded-btn bg-base-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-					{options.map((option, key) => (
-						<Listbox.Option
-							key={key}
-							className={({ active }) =>
-								`relative cursor-default select-none py-2 list-none ${
-									active ? 'bg-base-200' : 'text-base-content'
-								}`
-							}
-							value={option.value}
-						>
-							{({ selected }) => (
-								<>
-									<span
-										className={`block truncate ${
-											selected ? 'ml-8 font-medium' : 'ml-2 font-normal'
-										} ${ option.value === null && 'text-base-content/40' }`}
-									>
-										{displayFn ? displayFn( option ) : option?.title}
-									</span>
-									{selected ? (
-										<span className="absolute inset-y-0 left-0 flex items-center text-primary ml-2">
-											<FontAwesomeIcon size="xs" icon={faCheck} className="h-5 w-5" aria-hidden="true" />
+	return (
+		<Listbox value={selectedValue} onChange={onSelectChange} name={name} {...props}>
+			<div className="relative">
+				<Listbox.Button className={classNames}>
+					<span className="h-full flex items-center truncate overflow-hidden">
+						{(placeholder && !selectedOption) && <span className="text-sm text-gray-400 font-normal h-full flex items-center truncate">{placeholder}</span>}
+						<span className="mx-1">{(displayFn && selectedOption) ? displayFn(selectedOption) : selectedOption?.title}</span>
+					</span>
+				</Listbox.Button>
+				{err && <label className="label">
+					{typeof err == 'string' && <span className="label-text-alt text-error">{err}</span>}
+				</label>}
+				<Transition
+					as={Fragment}
+					leave="transition ease-in duration-100"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<Listbox.Options className="absolute z-30 mt-1 pl-0 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+						<div className="p-2">
+							<input
+								type="text"
+								value={searchQuery}
+								onChange={(e) => setSearchQuery(e.target.value)}
+								placeholder="Search..."
+								className="input input-bordered w-full"
+							/>
+						</div>
+						{filteredOptions.map((option, key) => (
+							<Listbox.Option
+								key={key}
+								className={({ active }) => `relative cursor-default select-none py-2 list-none ${active ? 'bg-base-200' : 'text-neutral'}`}
+								value={option.value}
+							>
+								{({ selected }) => (
+									<>
+										<span className={`block truncate ${selected ? 'ml-8 font-medium' : 'ml-2 font-normal'} ${option.value === null && 'text-neutral-400'}`}>
+											{displayFn ? displayFn(option) : option?.title}
 										</span>
-									) : null}
-								</>
-							)}
-						</Listbox.Option>
-					))}
-				</Listbox.Options>
-			</Transition>
-		</div>
-	</Listbox>;  
+										{selected ? (
+											<span className="absolute inset-y-0 left-0 flex items-center text-primary ml-2">
+												<FontAwesomeIcon size="xs" icon={faCheck} className="h-5 w-5" aria-hidden="true" />
+											</span>
+										) : null}
+									</>
+								)}
+							</Listbox.Option>
+						))}
+					</Listbox.Options>
+				</Transition>
+			</div>
+		</Listbox>
+	);
 } );
+
 
 Select.displayName = 'Select';
 
